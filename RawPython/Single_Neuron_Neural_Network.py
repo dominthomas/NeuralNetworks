3473d06157bbee58012140553a220a4152816f29
Domin <dominthomas@gmail.com>
2020-03-04 13:56:06 +0000

    Multi Layer NN

--- RawPython/Single_Neuron_Neural_Network.py
+++ RawPython/Single_Neuron_Neural_Network.py
@@ -251,6 +251,6 @@ def model(X_train, Y_train, X_test, Y_test, num_iterations=2000, learning_rate=0
     return d
 
 
-d = model(train_set_x, train_set_y, test_set_x, test_set_y, num_iterations=2000, learning_rate=0.005, print_cost=True)
+d = model(train_set_x, train_set_y, test_set_x, test_set_y, num_iterations=100 , learning_rate=0.005, print_cost=True)
 # Best # of iterations = 100
 
